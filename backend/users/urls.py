43c615d90522f85ecfacb9f76d57346db7fc2ebe
osiriss <osiriss@bk.ru>
2022-11-17 20:07:29 +0700

    1

---
+++ backend/users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from django.contrib.auth.views import PasswordResetView
+
+from .views import UserViewSetForRequests
+
+app_name = 'api'
+
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSetForRequests)
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+]
