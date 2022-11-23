0a748e580224b81afac28ee48c5e5c2784494f47
osiriss <osiriss@bk.ru>
2022-11-23 18:16:38 +0700

    review 3

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,9 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny
+
 from ..api.user_serializer import RecipeForUserSerializer
+
 from .filters import IngredientFilter, RecipeFilter
 from backend.recipes.models import Ingredient, Recipe, Tag
 from .permissions import AuthPostAuthorChangesOrReadOnly
